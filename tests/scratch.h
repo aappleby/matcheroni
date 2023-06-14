240cb9bd4d02fb291a0a59140ca6572ddd78f70c
Austin Appleby <aappleby@gmail.com>
2023-06-14 01:30:41 -0700

    checkpoint

--- tests/scratch.h
+++ tests/scratch.h
@@ -15,10 +15,10 @@ enum frogs {
 void glark() {
 }
 
-static int flerpbo = {1, 2, 3, -7.0e12};
+static int flerpbo[] = {1, 2, 3, -7.0e12};
 
-static const int* y[const int 7];
-static const int* x[7] = {1+1*2/3, 2, 3};
+static const int* y[7];
+static const int x[7] = {1+1*2/3, 2, 3};
 
 struct glarp {
   int x;
