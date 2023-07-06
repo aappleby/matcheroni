2b1279f7a50bdb36bedf7e509ba46002826583bc
Austin Appleby <aappleby@gmail.com>
2023-07-06 02:16:55 -0700

    bugfixes

--- matcheroni/regex_parser.cpp
+++ matcheroni/regex_parser.cpp
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <vector>
 
+//------------------------------------------------------------------------------
+
 struct Node {
   const char* type;
   const char* a;
@@ -18,14 +20,43 @@ struct Node {
   std::vector<Node*> children;
 };
 
-std::vector<Node*> node_stack;
+//------------------------------------------------------------------------------
+
+struct FactoryState {
+  inline static std::vector<Node*> node_stack;
+};
+
+template<StringParam type, typename P, typename NodeType>
+struct Factory : public FactoryState {
+  static const char* match(void* ctx, const char* a, const char* b) {
+    if (!a || a == b) return nullptr;
+
+    auto old_top = node_stack.size();
+    auto end = P::match(ctx, a, b);
+    auto new_top = node_stack.size();
+
+    if (end) {
+      auto new_node = new NodeType();
+      new_node->type = type.str_val;
+      new_node->a = a;
+      new_node->b = end;
+      for (int i = old_top; i < new_top; i++) {
+        new_node->children.push_back(node_stack[i]);
+      }
+      node_stack.resize(old_top);
+      node_stack.push_back(new_node);
+    }
+
+    return end;
+  }
+};
 
 template<>
 inline void atom_rewind(void* ctx, const char* a, const char* b) {
   //printf("rewind to {%-20.20s}\n", a);
-  while(node_stack.size() && node_stack.back()->b > a) {
-    delete node_stack.back();
-    node_stack.pop_back();
+  while(FactoryState::node_stack.size() && FactoryState::node_stack.back()->b > a) {
+    delete FactoryState::node_stack.back();
+    FactoryState::node_stack.pop_back();
   }
 }
 
@@ -57,60 +88,35 @@ struct Trace : public TraceState {
 
 //------------------------------------------------------------------------------
 
-template<StringParam type, typename P, typename NodeType>
-struct Factory {
-  static const char* match(void* ctx, const char* a, const char* b) {
-    if (!a || a == b) return nullptr;
-
-    auto old_top = node_stack.size();
-    auto end = P::match(ctx, a, b);
-    auto new_top = node_stack.size();
-
-    if (end) {
-      auto new_node = new NodeType();
-      new_node->type = type.str_val;
-      new_node->a = a;
-      new_node->b = end;
-      for (int i = old_top; i < new_top; i++) {
-        new_node->children.push_back(node_stack[i]);
-      }
-      node_stack.resize(old_top);
-      node_stack.push_back(new_node);
-    }
-
-    return end;
-  }
-};
-
-//------------------------------------------------------------------------------
-
+#ifdef TRACE
 template<StringParam type, typename P>
 using Capture = Factory<type, Trace<type, P>, Node>;
-//using Capture = Factory<type, P>;
-
-template<auto t> using RAtom    = Capture<"ratom",    Atom<t>>;
-template<auto t> using NotRAtom = Capture<"notratom", NotAtom<t>>;
-template<auto t> using AnyRAtom = Capture<"anyratom", AnyAtom>;
+#else
+template<StringParam type, typename P>
+using Capture = Factory<type, P, Node>;
+#endif
 
 const char* match_regex(void* ctx, const char* a, const char* b);
 using regex = Ref<match_regex>;
 
-using meta      = Capture<"meta",     Seq<RAtom<'\\'>, AnyAtom>>;
-using rchar     = Capture<"rchar",    Oneof<NotAtom<'\\', ')', '|', '$', '.', '+', '*', '?'>, meta>>;
-using range     = Capture<"range",    Seq<rchar, RAtom<'-'>, rchar>>;
-using set_item  = Capture<"set_item", Oneof<range, rchar>>;
-using pos_set   = Capture<"pos_set",  Seq<RAtom<'['>, Some<set_item>, RAtom<']'>>>;
-using neg_set   = Capture<"neg_set",  Seq<RAtom<'['>, RAtom<'^'>, Some<set_item>, RAtom<']'>>>;
-using set       = Capture<"set",      Oneof<neg_set, pos_set>>; // Negative first, so we catch the ^!
-using eos       = Capture<"eos",      RAtom<'$'>>;
-using any       = Capture<"any",      RAtom<'.'>>;
-using group     = Capture<"group",    Seq<RAtom<'('>, regex, RAtom<')'>>>;
-using unit      = Capture<"unit",     Oneof<group, any, eos, rchar, set>>;
-using star      = Capture<"star",     Seq<unit, RAtom<'*'>>>;
-using plus      = Capture<"plus",     Seq<unit, RAtom<'+'>>>;
-using opt       = Capture<"opt",      Seq<unit, RAtom<'?'>>>;
-using simple    = Capture<"simple",   Some<star, plus, opt, unit>>;
-using regex_top = Capture<"regex",    Seq< simple, Opt<Seq< RAtom<'|'>, regex >> > >;
+using meta      = Capture<"meta",      Seq<Atom<'\\'>, AnyAtom>>;
+using rchar     = Capture<"rchar",     Oneof<NotAtom<'\\', '(', ')', '|', '$', '.', '+', '*', '?', '[', ']', '^'>, meta>>;
+using text      = Capture<"text",      Some<rchar>>;
+using range     = Capture<"range",     Seq<rchar, Atom<'-'>, rchar>>;
+using set_item  = Capture<"set_item",  Oneof<range, rchar>>;
+using pos_set   = Capture<"pos_set",   Seq<Atom<'['>, Some<set_item>, Atom<']'>>>;
+using neg_set   = Capture<"neg_set",   Seq<Atom<'['>, Atom<'^'>, Some<set_item>, Atom<']'>>>;
+using set       = Capture<"set",       Oneof<neg_set, pos_set>>; // Negative first, so we catch the ^!
+using eol       = Capture<"eos",       Atom<'$'>>;
+using dot       = Capture<"dot",       Atom<'.'>>;
+using group     = Capture<"group",     Seq<Atom<'('>, regex, Atom<')'>>>;
+using unit      =                      Oneof<group, dot, eol, text, set>;
+using star      = Capture<"star",      Seq<unit, Atom<'*'>>>;
+using plus      = Capture<"plus",      Seq<unit, Atom<'+'>>>;
+using opt       = Capture<"opt",       Seq<unit, Atom<'?'>>>;
+using simple    =                      Some<star, plus, opt, unit>;
+using alternate = Capture<"alternate", Seq< simple, Some<Seq< Atom<'|'>, simple >> > >;
+using regex_top = Capture<"regex",     Oneof< alternate, simple > >;
 
 const char* match_regex(void* ctx, const char* a, const char* b) {
   return regex_top::match(ctx, a, b);
@@ -123,20 +129,16 @@ int main(int argc, char** argv) {
   if (argc == 2) printf("argv[1] = %s\n", argv[1]);
 
   const char* regex = argv[1];
-  //regex = "[^a-c][b-d]";
-  //regex = "a|b|c";
-
+  //regex = "ab|bc|cd";
   auto end = regex::match(nullptr, regex, regex + + strlen(regex));
 
-  printf("node stack size %ld\n", node_stack.size());
-  printf("\n");
-  printf("parse tree:\n");
-  if (node_stack.size()) {
-    for (auto n : node_stack) n->dump();
+  if (FactoryState::node_stack.size()) {
+    printf("Parse tree:\n");
+    for (auto n : FactoryState::node_stack) n->dump();
   }
-
-  printf("end = %s\n", end);
-
+  else {
+    printf("Parse failed!\n");
+  }
 }
 
 //------------------------------------------------------------------------------
