23a3fc3ce9b7c40c5ba2a7f474f60caee456781a
Austin Appleby <aappleby@gmail.com>
2023-07-13 14:10:49 -0700

    begin/end capture works

--- tests/parseroni_test.cpp
+++ tests/parseroni_test.cpp
@@ -46,60 +46,65 @@ static int fail_count = 0;
 
 //------------------------------------------------------------------------------
 
-struct BackcapTest {
+struct BeginEndTest {
   static cspan match(void* ctx, cspan s) {
-    return pattern::match(ctx, s);
+    return Oneof<atom, list>::match(ctx, s);
   }
 
-  using pattern =
-  Oneof<
-    CaptureNamed<"atom", Range<'a','z'>, TestNode>,
-    Seq<
-      CaptureNamed<"match", Seq<Atom<'['>, Ref<match>, Atom<']'>>, TestNode>,
-      Oneof<
-        BackCap<1, "plus",  Atom<'+'>, TestNode>,
-        BackCap<1, "minus", Atom<'-'>, TestNode>,
-        BackCap<1, "star",  Atom<'*'>, TestNode>,
-        BackCap<1, "slash", Atom<'/'>, TestNode>,
-        BackCap<1, "opt",   Atom<'?'>, TestNode>,
-        BackCap<1, "eq",    Atom<'='>, TestNode>,
-        BackCap<1, "none",  Nothing,   TestNode>
-      >
+  using ws = Any<Atom<' ', '\n', '\r', '\t'>>;
+
+  template<typename P>
+  using suffixed =
+  CaptureBegin<
+    P,
+    Opt<
+      Seq<Atom<'+'>,  CaptureEnd<"plus",  TestNode>>,
+      Seq<Atom<'*'>,  CaptureEnd<"star",  TestNode>>,
+      Seq<Atom<'?'>,  CaptureEnd<"opt",   TestNode>>
     >
   >;
+
+  using atom =
+  suffixed<CaptureNamed<
+    "atom",
+    Some<Range<'a','z'>, Range<'A','Z'>, Range<'0','9'>>,
+    TestNode
+  >>;
+
+  using car = Opt<Ref<match>>;
+  using cdr = Any<Seq<ws, Atom<','>, ws, Ref<match>>>;
+
+  using list =
+  suffixed<CaptureNamed<
+    "list",
+    Seq<Atom<'['>, ws, car, cdr, ws, Atom<']'>>,
+    TestNode
+  >>;
 };
 
-void test_backcap() {
-  Context p;
-  cspan span;
-  cspan tail;
+void test_begin_end() {
+  printf("test_begin_end()\n");
 
-  // "a"   - cap 8 create 1
-  // "[a]" - cap 63 create 8
-  // "[[a]]" - cap 448 create 57
-  // "[[[a]]]" - cap 3143 create 400
-  // "[[[[a]]]]" - cap 22008 create 2801
-  // "[[[[[a]]]]]" - cap 154063 create 19608
-  // "[[[[[[a]]]]]]" - cap 1078448 create 137257
+  auto text = to_span("[ [abc,ab?,cdb+] , [a,b,c*,d,e,f] ]");
 
-  //std::string text = "[[[a]]]";
-  std::string text = "[[[[[[a]]]]]]";
+  Context c;
+  auto tail = BeginEndTest::match(&c, text);
 
-  p.reset();
-  span = to_span(text);
-  tail = BackcapTest::match(&p, span);
-  CHECK(tail.is_valid(), "pathological tree invalid");
-  print_tree(p.top_head);
-  printf("sizeof(TestNode) %ld\n", sizeof(TestNode));
-  printf("node count %ld\n", p.top_head->node_count());
-  printf("constructor calls %ld\n", NodeBase::constructor_calls);
-  printf("destructor calls %ld\n", NodeBase::destructor_calls);
-  printf("max size %d\n", NodeBase::slabs.max_size);
-  printf("current size %d\n", NodeBase::slabs.current_size);
+  if (tail.is_valid()) {
+    print_match(text, text - tail);
+    if (c.top_head) print_tree(c.top_head);
+  }
+  else {
+    printf("invalid\n");
+  }
+
+  printf("test_begin_end() done\n");
+  printf("\n");
 }
 
 //------------------------------------------------------------------------------
 
+#if 0
 struct Pathological {
   static cspan match(void* ctx, cspan s) {
     return pattern::match(ctx, s);
@@ -119,7 +124,7 @@ struct Pathological {
 };
 
 void test_pathological() {
-  Context p;
+  Context c;
   cspan span;
   cspan tail;
 
@@ -134,18 +139,19 @@ void test_pathological() {
   //std::string text = "[[[a]]]";
   std::string text = "[[[[[[a]]]]]]";
 
-  p.reset();
+  c.reset();
   span = to_span(text);
-  tail = Pathological::match(&p, span);
+  tail = Pathological::match(&c, span);
   CHECK(tail.is_valid(), "pathological tree invalid");
-  print_tree(p.top_head);
+  print_tree(c.top_head);
   printf("sizeof(TestNode) %ld\n", sizeof(TestNode));
-  printf("node count %ld\n", p.top_head->node_count());
+  printf("node count %ld\n", c.top_head->node_count());
   printf("constructor calls %ld\n", NodeBase::constructor_calls);
   printf("destructor calls %ld\n", NodeBase::destructor_calls);
   printf("max size %d\n", NodeBase::slabs.max_size);
   printf("current size %d\n", NodeBase::slabs.current_size);
 }
+#endif
 
 //------------------------------------------------------------------------------
 // A whole s-expression parser in ~10 lines of code. :D
@@ -168,35 +174,35 @@ struct TinyLisp {
 //------------------------------------------------------------------------------
 
 void test_basic() {
-  Context p;
+  Context c;
   cspan span;
   cspan tail;
 
   {
     // Check than we can round-trip a s-expression
     std::string text = "(abcd,efgh,(ab),(a,(bc,de)),ghijk)";
-    p.reset();
+    c.reset();
     span = to_span(text);
-    tail = TinyLisp::match(&p, span);
+    tail = TinyLisp::match(&c, span);
     CHECK(tail.is_valid() && tail == "");
     std::string dump;
-    ((TestNode*)p.top_head)->dump_tree(dump);
+    ((TestNode*)c.top_head)->dump_tree(dump);
     CHECK(text == dump, "Round trip s-expression");
   }
 
-  p.reset();
+  c.reset();
   span = to_span("((((a))))");
-  tail = TinyLisp::match(&p, span);
+  tail = TinyLisp::match(&c, span);
   CHECK(tail.is_valid() && tail == "");
 
-  p.reset();
+  c.reset();
   span = to_span("(((())))");
-  tail = TinyLisp::match(&p, span);
+  tail = TinyLisp::match(&c, span);
   CHECK(tail.is_valid() && tail == "");
 
-  p.reset();
+  c.reset();
   span = to_span("(((()))(");
-  tail = TinyLisp::match(&p, span);
+  tail = TinyLisp::match(&c, span);
   CHECK(!tail.is_valid() && tail == "(");
 }
 
@@ -206,8 +212,9 @@ int main(int argc, char** argv) {
   printf("Parseroni tests\n");
 
   //test_basic();
-  test_backcap();
-  test_pathological();
+  test_begin_end();
+  //test_backcap();
+  //test_pathological();
 
   if (!fail_count) {
     printf("All tests pass!\n");
